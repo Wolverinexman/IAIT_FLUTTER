fc94f5956a9dec4a633521a8fc9b90a457f61ca9
Wolverinexman <mujju3699@gmail.com>
2024-05-04 11:49:48 +0530

    first commit

---
+++ iait1/lib/Screens/otp_screen.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+class OtpScreen extends StatefulWidget {
+  final String verificationId;
+
+  const OtpScreen({super.key, required this.verificationId});
+
+  @override
+  State<OtpScreen> createState() => _OtpScreenState();
+}
+
+class _OtpScreenState extends State<OtpScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 25, horizontal: 30),
+            child: Column(
+              children: [
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: GestureDetector(
+                    onTap: () => Navigator.of(context).pop(),
+                    child: const Icon(Icons.arrow_back),
+                  ),
+                ),
+                Container(
+                  width: 200,
+                  height: 200,
+                  padding: const EdgeInsets.all(20.0),
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.purple.shade50,
+                  ),
+                  child: Image.asset(
+                    "asset/image2.png",
+                  ),
+                ),
+                const SizedBox(height: 20),
+                const Text(
+                  "Verification",
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 10),
+                const Text(
+                  "Enter the otp sent to your phone number",
+                  style: TextStyle(
+                      fontSize: 14,
+                      color: Colors.black38,
+                      fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 20),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
